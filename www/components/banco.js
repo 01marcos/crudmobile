5f346310a547d00503f84c70ac65b31f7bf6b91c
01marcos <42778662+01marcos@users.noreply.github.com>
2019-04-13 02:54:28 +0900

    Project's Initial Commit

---
+++ www/components/banco.js
@@ -0,0 +1,22 @@
+// This is a JavaScript file
+
+$(document).on('click','#cadastro', function(){
+  var parametros = {
+    "nome": $("#nome").val(),
+    "email": $("#email").val()
+  }
+
+  $.ajax({
+    type:"post",
+    url:"https://crud2019-marcosvini001.c9users.io/cadastro.php",
+    data:parametros,
+    sucess:function(data){
+      $("#nome").val(""); 
+      $("#email").val("");
+      navigator.notification.alert(data);
+      },
+      error:function(data){
+        navigator.notification.alert(data);
+      }
+  });
+});
